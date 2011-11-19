8f9dbb03db326d272fa58370cbee055940adfcd7
Alex Grant <alex@grantovich.net>
2011-11-19 14:17:45 -0500

    Always use SSL

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 gem 'sinatra'
 gem 'haddock'
+gem 'rack-ssl-enforcer'
--- haddock-heroku.rb
+++ haddock-heroku.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'haddock'
 
+configure :production do
+  require 'rack-ssl-enforcer'
+  use Rack::SslEnforcer
+end
+
 get '/' do
   'It works!'
 end
