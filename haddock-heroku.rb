5987cb6ee23730d2e158fae563b5ac595faee761
Alex Grant <alex@grantovich.net>
2011-11-19 19:56:03 -0500

    Generate multiple passwords of specified length

--- haddock-heroku.rb
+++ haddock-heroku.rb
@@ -8,9 +8,19 @@ configure :production do
   use Rack::SslEnforcer
 end
 
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 set :views, settings.root
 
 get '/' do
-  @password = Haddock::Password.generate(16)
+  @length = params[:length].to_i
+  @length = 16 if @length <= 0
+  @length = 8 if @length < 8
+  @length = 31 if @length > 31
+  @passwords = []
+  10.times{ @passwords << Haddock::Password.generate(@length) }
   erb :index
 end
